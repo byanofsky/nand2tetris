a66e9dddf5daed143544da55b251d01eb7ae2635
byanofsky <byanofsky@me.com>
2019-08-17 10:13:56 -0700

    Project 4 solutions

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,43 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+    // Initialize values
+    @SCREEN
+    D=A
+    @pos
+    M=D  // pos = SCREEN
+    @color
+    M=0  // color = white
+
+    @KBD
+    D=M  // D = M[KBD]
+    @SCREENLOOP
+    D;JEQ  // if (M[KBD] == 0) goto SCREENLOOP
+
+    // If Key pressed, change color to black
+    @color
+    M=-1  // color = black
+
+(SCREENLOOP)
+    // Set pixels
+    @color
+    D=M  // D = color
+    @pos
+    A=M
+    M=D  // M[pos] = color
+    @pos
+    M=M+1  // pos = pos + 1
+
+    // If (pos < SCREEN + 8192) goto SCREENLOOP
+    @8192  // (256 * 32)
+    D=A
+    @SCREEN
+    D=D+A  // D = 8192 + SCREEN
+    @pos
+    D=M-D  // D = pos - (8192 + SCREEN)
+    @SCREENLOOP
+    D;JLT  // if (pos < SCREEN + 8192) goto SCREENLOOP
+
+    @LOOP
+    0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,29 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @n
+    M=0  // n = 0
+    @R2
+    M=0  // M[R2] = 0
+
+(LOOP)
+    @n
+    D=M  // D = n
+    @R1
+    D=D-M  // D = n - M[R1]
+    @END
+    D;JGE  // if (n > M[R1]) goto END
+
+    @R0
+    D=M
+    @R2
+    M=M+D  // M[R2] = M[R2] + M[R0] 
+    @n
+    M=M+1  // n = n + 1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
