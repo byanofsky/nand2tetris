dc06b5f71ace7356ab0eca3c9b165289f3299d9d
byanofsky <byanofsky@me.com>
2019-09-01 10:44:47 -0700

    Project 04 redo for course part 2

--- 04-02/fill/Fill.asm
+++ 04-02/fill/Fill.asm
@@ -11,3 +11,52 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
+// reset
+(LOOP)
+    @SCREEN
+    D=A
+    @addr
+    M=D  // addr=SCREEN
+    @KBD
+    D=M
+    @BLACK_OUT
+    D;JGT  // goto BLACK_OUT if M[KBD] > 0
+
+// paint white
+    @color
+    D=M
+    @LOOP
+    D;JEQ  // if color==0, screen is already white, so don't repaint
+    @color
+    M=0  // color=0 (white)
+    @FILL
+    0;JMP  // Jump to fill
+
+// paint black
+(BLACK_OUT)
+    @color
+    D=M
+    @LOOP
+    D+1;JEQ  // if color ==-1, screen is already black, so don't repaint
+    @color
+    M=-1  // color=-1 (black)
+
+// paint screen
+(FILL)
+    @addr
+    D=M
+    @24576  // 16384 + 32*256
+    D=D-A
+    @LOOP
+    D;JGE  // goto LOOP if addr >= 8192
+
+    @color
+    D=M
+    @addr
+    A=M
+    M=D  // M[addr]=color
+    @addr
+    M=M+1  // addr+=1
+
+    @FILL
+    0;JMP
--- 04-02/mult/mult.asm
+++ 04-02/mult/mult.asm
@@ -6,3 +6,29 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
+    @i
+    M=0  // i=0
+    @R2
+    M=0  // M[R2]=0
+
+(LOOP)
+    @i
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JGE  // goto END if i >= M[R1]
+
+    @R0
+    D=M
+    @R2
+    M=D+M  // M[R2]+=M[R0]
+    @i
+    M=M+1  // i++
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP  // terminating loop
