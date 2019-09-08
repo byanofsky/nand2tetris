d7040d095a02c305ad66a672855521105c812f52
byanofsky <byanofsky@me.com>
2019-09-08 09:57:19 -0700

    Add memory access

--- 07/VMTranslator/src/lib/Commands.ts
+++ 07/VMTranslator/src/lib/Commands.ts
@@ -48,22 +48,118 @@ const createConditionCommand = (() => {
   };
 })();
 
-export const pushCommand = ({ segment, index }: Token) => {
-  let value;
+const memMap = {
+  [Segments.constant]: 9,
+  [Segments.local]: 1,
+  [Segments.argument]: 2,
+  [Segments.this]: 3,
+  [Segments.that]: 4,
+  [Segments.pointer]: 3,
+  [Segments.temp]: 5
+};
+
+export const pushCommand = ({ segment, index, baseName }: Token) => {
+  if (index === undefined) {
+    throw new Error('push command with invalid index: ' + index);
+  }
   switch (segment) {
     case Segments.constant:
-      value = index;
-      break;
+      return [`@${index}`, 'D=A', '@SP', 'A=M', 'M=D', ...incStack].join('\n');
+    case Segments.local:
+    case Segments.argument:
+    case Segments.this:
+    case Segments.that: {
+      return [
+        `@${memMap[segment]}`,
+        'D=M',
+        `@${index}`,
+        'A=D+A',
+        'D=M',
+        '@SP',
+        'A=M',
+        'M=D',
+        ...incStack
+      ].join('\n');
+    }
+    case Segments.pointer:
+    case Segments.temp: {
+      return [
+        `@${memMap[segment] + index}`,
+        'D=M',
+        '@SP',
+        'A=M',
+        'M=D',
+        ...incStack
+      ].join('\n');
+    }
+    case Segments.static: {
+      return [
+        `@${baseName}.${index}`,
+        'D=M',
+        '@SP',
+        'A=M',
+        'M=D',
+        ...incStack
+      ].join('\n');
+    }
     default:
       throw new Error('unrecognized segment: ' + segment);
   }
-  return [`@${value}`, 'D=A', '@SP', 'A=M', 'M=D', ...incStack].join('\n');
 };
 
-export const popCommand = ({ segment, index }: Token) => {
+export const popCommand = ({ segment, index, baseName }: Token) => {
+  if (index === undefined) {
+    throw new Error('push command with invalid index: ' + index);
+  }
+
   switch (segment) {
     case Segments.constant:
       throw new Error('cannot pop for segment: ' + Segments.constant);
+    case Segments.local:
+    case Segments.argument:
+    case Segments.this:
+    case Segments.that: {
+      return [
+        '@SP',
+        'M=M-1',
+        'A=M',
+        'D=M',
+        '@R13',
+        'M=D',
+        `@${memMap[segment]}`,
+        'D=M',
+        `@${index}`,
+        'D=A+D', // addr pop to
+        '@R14',
+        'M=D',
+        '@R13',
+        'D=M',
+        '@R14',
+        'A=M',
+        'M=D'
+      ].join('\n');
+    }
+    case Segments.pointer:
+    case Segments.temp: {
+      return [
+        '@SP',
+        'M=M-1',
+        'A=M',
+        'D=M',
+        `@${memMap[segment] + index}`,
+        'M=D'
+      ].join('\n');
+    }
+    case Segments.static: {
+      return [
+        '@SP',
+        'M=M-1',
+        'A=M',
+        'D=M',
+        `@${baseName}.${index}`,
+        'M=D'
+      ].join('\n');
+    }
     default:
       throw new Error('unrecognized segment: ' + segment);
   }
