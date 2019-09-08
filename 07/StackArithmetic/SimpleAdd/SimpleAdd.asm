ac0e5c3b727eae636e26921007ae4bfb2a68c7d2
byanofsky <byanofsky@me.com>
2019-09-08 10:35:44 -0700

    Project 07 solution asm and output

---
+++ 07/StackArithmetic/SimpleAdd/SimpleAdd.asm
@@ -0,0 +1,30 @@
+// 7: push constant 7
+@7
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// 8: push constant 8
+@8
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// 9: add
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=M+D
+@SP
+M=M+1
+
